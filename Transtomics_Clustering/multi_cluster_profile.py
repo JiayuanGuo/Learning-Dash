5c4f4358a8d8450dc4b4e8cda19ffca82b171e0b
Jiayuan Guo <jyguo@uw.edu>
2018-02-25 13:46:38 -0800

    multi-output

--- Transtomics_Clustering/multi_cluster_profile.py
+++ Transtomics_Clustering/multi_cluster_profile.py
@@ -12,12 +12,45 @@ app = dash.Dash()
 app.layout = html.Div([
         dcc.Input(id='k-value', value= 15, type='number'),
         dcc.Input(id='cluster-id', value = 0, type = 'number'),
+    dcc.Graph(id='graph-cluster-profile'),
     dcc.Graph(id='graph-cluster-size')
     ])
 
 
 @app.callback(
     dash.dependencies.Output('graph-cluster-size', 'figure'),
+    [dash.dependencies.Input(component_id='k-value',component_property='value')]
+)
+
+def cluster_size_figure(kvalue):
+    X = df
+
+    kmeans = KMeans(n_clusters=kvalue, max_iter=300, random_state=4)
+    kmeans.fit(X)
+
+    labels_kmeans = kmeans.labels_
+    df_clusterid = pd.DataFrame(labels_kmeans, index=df.index)
+    df_clusterid.rename(columns={0: "cluster"}, inplace=True)
+    df_clusters = pd.concat([df, df_clusterid], axis=1)
+
+    count = df_clusters.groupby('cluster').count().iloc[:, 0]
+
+    return {
+        'data':[go.Bar(
+            x = list(count.index),
+            y = count.values
+        )],
+        'layout': go.Layout(
+            xaxis = {'title':'cluster id'},
+            yaxis = {'title':'number of genes in each cluster'},
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+            hovermode='closest'
+        )
+    }
+
+
+@app.callback(
+    dash.dependencies.Output('graph-cluster-profile', 'figure'),
     [dash.dependencies.Input(component_id='k-value',component_property='value'),
      dash.dependencies.Input(component_id='cluster-id', component_property='value')]
 )
